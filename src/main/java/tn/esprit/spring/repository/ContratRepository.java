e3e7bb50711b9b495afe7f545354974f38154f46
youssefe1 <48757653+youssefe1@users.noreply.github.com>
2021-11-02 17:38:09 +0100

    Test
    Test

--- src/main/java/tn/esprit/spring/repository/ContratRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import tn.esprit.spring.entities.Contrat;
-
-public interface ContratRepository extends CrudRepository<Contrat, Integer>{
-
-} 
